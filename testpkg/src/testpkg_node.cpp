74e90cee1e9c283ad833db7cf9928659e3edafda
ibrahiminfinite <ibrahimjkd@gmail.com>
2021-03-16 11:52:58 +0530

    add rrt

--- testpkg/src/testpkg_node.cpp
+++ testpkg/src/testpkg_node.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include "sampler/sampler_node.h"
-#include "graph/graph_node.h"
-
+// #include "graph/graph_node.h"
+#include "rrt/rrt.h"
+#include "kdtree/kdtree.h"
 
 int main()
 {
@@ -12,12 +13,18 @@ int main()
 
 
     //Graph Testing
-    auto g = graph::Graph();
-    auto v1 = Vector3(0.0f,0.0f,0.0f);
-    auto v2 = Vector3(3.0f,3.0f,3.0f);
-    g.addVertex(v1);
-    g.addVertex(v2);
-    g.addEdge(v1, v2);
-    std::cout<<g.hasVertex(v1)<<"\n";
+    // auto g = graph::Graph();
+    // auto v1 = Vector3(0.0f,0.0f,0.0f);
+    // auto v2 = Vector3(3.0f,3.0f,3.0f);
+    // g.addVertex(v1);
+    // g.addVertex(v2);
+    // g.addEdge(v1, v2);
+    // std::cout<<g.hasVertex(v1)<<"\n";
+
+
+    //RRT Testing
+    // rrt::init_rrt(v1);
+
     return 0;
 }
+
